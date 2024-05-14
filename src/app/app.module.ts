23137b28971fc8e41aab3d6dd8dc9a123ce86298
DevMike08 <maescobarocampo@gmail.com>
2024-05-14 17:28:52 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './welcome/pages/home/home.component';
+import { NavigationComponent } from './shared/components/navigation/navigation/navigation.component';
+import { FooterComponent } from './shared/components/footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
